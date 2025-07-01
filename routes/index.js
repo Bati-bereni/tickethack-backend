ceb3114ff5a419e2b5b97aabfdfb6529f8d5aaac
{Your_name} <Your email>
2025-07-01 14:11:24 +0200

    router

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,56 @@
 var express = require("express");
 var router = express.Router();
 
-const data = require("../models/connection"); //import, initialisation connection
+require("../models/connection"); //import, initialisation connection
 const Trip = require("../models/trips"); //import du modèle
 // autant que de models/schemas
 
-/* GET home page. */
-// router.get('/', function(req, res, next) {
-//   res.render('index', { title: 'Express' });
+// /* GET home page. */
+// // router.get('/', function(req, res, next) {
+// //   res.render('index', { title: 'Express' });
+// // });
+
+// function getAllTrips() {
+//   Trip.find().then((data) => {
+//     console.log(data);
+//   });
+// }
+
+// router.get("/getAllTrips", (req, res) => {
+//   Trip.find().then((data) => {
+//     res.json({ allTrips: data });
+//   });
 // });
 
-function AllData() {
-  data.find().then((data) => {
-    console.log(data);
-  });
-}
+// router.post("/getOneTrip", (req, res) => {
+//   // console.log("body dans le back =>", req.body.depart);
 
-AllData();
+//   Trip.findOne({ departure: req.body.depart, arrival: req.body.arrivee })
+//   .then((data) => {
+//       res.json({ theTrip: data });
+//     }
+//   );
+// });
+
+
+// router.get("/maRoute", (req, res) => {
+
+//   res.json({message : "je suis dans ma route"})
+
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+// getAllTrips();
 
 // router.get('/trip', (req, res) {
 //   data.find().then(data => { console.log(data);});
@@ -36,5 +70,17 @@ AllData();
 //   // exploitation du document créé
 //   console.log(newDoc);
 // });
+
+//-------------------------------//
+
+
+
+
+
+
+
+
+
+
 
 module.exports = router;
