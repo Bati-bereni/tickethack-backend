77d99ed232ae7df43c837b8bd4446196b39d8b7e
{Your_name} <Your email>
2025-07-01 12:02:20 +0200

    start backend

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,40 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+const data = require("../models/connection"); //import, initialisation connection
+const Trip = require("../models/trips"); //import du modèle
+// autant que de models/schemas
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+
+function AllData() {
+  data.find().then((data) => {
+    console.log(data);
+  });
+}
+
+AllData();
+
+// router.get('/trip', (req, res) {
+//   data.find().then(data => { console.log(data);});
+
+//  })
+// ;
+
+// const newTrip = new Trip({
+//   //utilisation du modèle
+//   departure: String,
+//   arrival: String,
+//   date: Date,
+//   price: Number,
+// });
+
+// newTrip.save().then((newDoc) => {
+//   // exploitation du document créé
+//   console.log(newDoc);
+// });
 
 module.exports = router;
